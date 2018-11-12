2f45054278206df2a037d82357dc75450ca54b6b
kbomb6 <baumanfamily4@gmail.com>
2018-11-12 14:53:15 -0700

    Added files

---
+++ BoiseBusinessesAPI/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Web.Http.
+using Microsoft.AspNetCore.Mvc;
+using BoiseBusinessesAPI.Models;
+using BoiseBusinessesAPI.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BoiseBusinessesAPI.Controllers
+{
+    //[System.Web.Http.Route("api/contact")]
+    [Route("api/contact")]
+    public class ContactController : Controller
+    {
+        private ContactRepository contactRepository;
+        
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
---
+++ BoiseBusinessesAPI/Services/ContactRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using BoiseBusinessesAPI.Models;
+
+namespace BoiseBusinessesAPI.Services
+{
+    public class ContactRepository
+    {
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+            {
+            new Contact
+            {
+                Id = 1,
+                Name = "Micron"
+            },
+            new Contact
+            {
+                Id = 2,
+                Name = "HP"
+            },
+            new Contact
+            {
+                Id = 3,
+                Name = "BSU"
+            }, 
+            new Contact
+            {
+                Id = 4,
+                Name = "Simplot"
+            },
+            new Contact
+            {
+                Id = 5,
+                Name = "Alavita"
+            }, 
+            new Contact
+            {
+                Id = 6,
+                Name = "Tsheets"
+            },
+            new Contact
+            {
+                Id = 7,
+                Name = "Paylocity"
+            },
+            new Contact
+            {
+                Id = 8,
+                Name = "Clearwater Analytics"
+            }
+            };
+        }
+    }
+}
